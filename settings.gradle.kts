00421c063f9c28778c9c0c3baff491574d9fba77
Joe Fang <25814618+MinecraftFuns@users.noreply.github.com>
2025-06-11 10:52:41 -0400

    chore: init

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EAPConnect"
+include(":wear")
+include(":mobile")  // optional companion; user may skip install
