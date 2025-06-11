7913d49de29ad710fa146b000f5cfe479b172e27
Joe Fang <25814618+MinecraftFuns@users.noreply.github.com>
2025-06-11 15:28:01 -0400

    chore: WIP

---
+++ mobile/src/main/java/org/joefang/wearos/eap/theme/WiFiSuggestTheme.kt
@@ -0,0 +1,11 @@
+package org.joefang.wearos.eap.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WiFiSuggestTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        content = content
+    )
+}
---
+++ mobile/src/main/java/org/joefang/wearos/eap/ui/WiFiConfigScreen.kt
@@ -0,0 +1,56 @@
+package org.joefang.wearos.eap.ui
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Button
+
+@Composable
+fun WiFiConfigScreen(onSubmit: (String, String, String, String) -> Unit) {
+    var ssid by remember { mutableStateOf("") }
+    var identity by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var domain by remember { mutableStateOf("") }
+
+    Column(Modifier.fillMaxWidth()) {
+        OutlinedTextField(
+            value = ssid,
+            onValueChange = { ssid = it },
+            label = { Text("SSID") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(Modifier.height(4.dp))
+        OutlinedTextField(
+            value = identity,
+            onValueChange = { identity = it },
+            label = { Text("Identity") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(Modifier.height(4.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            visualTransformation = PasswordVisualTransformation(),
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(Modifier.height(4.dp))
+        OutlinedTextField(
+            value = domain,
+            onValueChange = { domain = it },
+            label = { Text("Domain") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(Modifier.height(8.dp))
+        Button(
+            onClick = { onSubmit(ssid, identity, password, domain) },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text("Add Network")
+        }
+    }
+}
